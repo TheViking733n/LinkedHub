f29e50fc004f46fa887bdff42114644462f97a7d
Anant Prakash Singh <anant733n@gmail.com>
2023-11-16 04:47:38 +0530

    added field views to post schema

--- post/models.py
+++ post/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author_id = models.CharField(max_length=100)
+    views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
