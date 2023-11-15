77eea09707badf17c191b065002b6b9cfb4acc56
Anant Prakash Singh <anant733n@gmail.com>
2023-11-15 14:27:10 +0530

    normalized database schema - 1NF

--- home/models.py
+++ home/models.py
@@ -3,13 +3,29 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    username = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     profile_pic = models.TextField(blank=True)
     bio = models.TextField(blank=True)
-    connections = models.TextField(blank=True)
-    requests = models.TextField(blank=True)
-    post_ids = models.TextField(blank=True)
+    # connections = models.TextField(blank=True)
+    # requests = models.TextField(blank=True)
+    # post_ids = models.TextField(blank=True)
     
     def __str__(self):
         return self.username
+
+
+class Connection(models.Model):
+    user1 = models.CharField(max_length=100)
+    user2 = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.user1 + ' - ' + self.user2
+
+
+class PendingRequest(models.Model):
+    sender = models.CharField(max_length=100)
+    receiver = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.sender + ' - ' + self.receiver
--- post/models.py
+++ post/models.py
@@ -7,8 +7,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author_id = models.CharField(max_length=100)
-    likes = models.IntegerField(default=0)
-    comment_ids = models.TextField()
+    # likes = models.IntegerField(default=0)
+    # comment_ids = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -19,8 +19,18 @@ class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
     content = models.TextField()
     author_id = models.CharField(max_length=100)
-    likes = models.IntegerField(default=0)
+    # likes = models.IntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.content
+
+
+class Like(models.Model):
+    item_id = models.IntegerField()
+    item_type = models.CharField(max_length=10)  # 'post' or 'comment'
+    user_id = models.CharField(max_length=100)   # username of the liker
+    
+    def __str__(self):
+        return self.item_type + ' ' + str(self.item_id) + ' liked by ' + self.user_id
+
