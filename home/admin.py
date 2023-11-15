40c3c57aa53325c0530a7aa19093e4acfabe1c3d
Anant Prakash Singh <anant733n@gmail.com>
2023-11-15 22:23:00 +0530

    register the tables on admin site

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, Connection, PendingRequest
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Connection)
+admin.site.register(PendingRequest)
