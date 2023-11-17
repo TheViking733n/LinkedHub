66d4137fcfffbe02da51867b8c947b0557d16e01
Anant Prakash Singh <anant733n@gmail.com>
2023-11-17 08:23:27 +0530

    added organization in UserProfile schema

--- home/models.py
+++ home/models.py
@@ -7,9 +7,7 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=100)
     profile_pic = models.TextField(blank=True)
     bio = models.TextField(blank=True)
-    # connections = models.TextField(blank=True)
-    # requests = models.TextField(blank=True)
-    # post_ids = models.TextField(blank=True)
+    organization = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return self.username
