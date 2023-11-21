635126d174b66fb3974ab2d49bc1a795cf412bbe
Anant Prakash Singh <anant733n@gmail.com>
2023-11-21 10:20:16 +0530

    update static files dirs

--- LinkedHub/settings.py
+++ LinkedHub/settings.py
@@ -133,13 +133,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
+import os.path  
+import sys
 
+PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 import os
 # For Vercel deployment
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
